7e394c841043ba520917131e360fe922e9666b05
Aasa-Christian <aasachristian@yahoo.com>
2020-09-22 18:28:49 -0400

    created component to hold form for both login and register credentials

---
+++ components/CredForm.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import {
+  View, Text, StyleSheet
+} from 'react-native';
+
+
+
+const CredForm = () => {
+  return(
+    <View>
+        <View style={styles}>
+
+        </View>
+      
+        <View style={styles}>
+
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+
+})
+
+export default CredForm;
