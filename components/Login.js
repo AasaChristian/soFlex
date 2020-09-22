9296d8038e7f803662110dcd30555818e2a5378a
Aasa-Christian <aasachristian@yahoo.com>
2020-09-22 18:28:06 -0400

    created login componenet

---
+++ components/Login.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import {
+  View, Text, StyleSheet
+} from 'react-native';
+
+
+
+const Login = () => {
+  return(
+    <View>
+        <View style={styles.nav}>
+
+        </View>
+      
+        <View style={styles.header}>
+
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+
+})
+
+export default Login;
