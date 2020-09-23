b2b976b3bcd8881d6c29f43cd9184ec00eac5275
Aasa-Christian <aasachristian@yahoo.com>
2020-09-23 17:08:49 -0400

    add navigation functionality allows login button to go to credform

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   View, Text
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+// import Components
 import CredForm from './components/CredForm';
 import Head from './components/Head'
 import Login from './components/Login'
 
 
+const Stack = createStackNavigator()
 
 const App = () => {
   return(
-    <View>
+    <NavigationContainer>
       <Head/>
-      {/* <Login/> */}
-      <CredForm/>
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{title: null}}
+        />
+
+        <Stack.Screen
+        name="CredForm"
+        component={CredForm}
+        options={{title: null}}
+        
+        />
+
+      </Stack.Navigator>
+      
+    </NavigationContainer>
+    
   )
 }
 
