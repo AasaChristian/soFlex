be4e57eabee59121bd013c5d42568289390fdb57
Aasa-Christian <aasachristian@yahoo.com>
2020-10-15 14:19:38 -0400

    created component to show users personal stats

---
+++ components/PersonalStats.js
@@ -0,0 +1,27 @@
+
+import React, {useState} from 'react';
+import {
+  View, Text, StyleSheet, SafeAreaView, FlatList,TouchableOpacity
+} from 'react-native';
+
+
+
+const PersonalStats = () => {
+
+
+  return(
+    <View style={styles.container}>
+        <Text>Personal details</Text>
+    </View>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 100
+    },
+
+})
+
+export default PersonalStats;
